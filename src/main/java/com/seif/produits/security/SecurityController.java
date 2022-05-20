c1547ff3420700fb359b7bc48a6be5f94e023bc8
unknown <seifeddinebissa1@gmail.com>
2022-05-20 17:37:03 +0100

    first commit

---
+++ src/main/java/com/seif/produits/security/SecurityController.java
@@ -0,0 +1,15 @@
+package com.seif.produits.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+	@GetMapping("/accessDenied")
+	public String error()
+	{
+	return "accessDenied";
+	}
+
+}
